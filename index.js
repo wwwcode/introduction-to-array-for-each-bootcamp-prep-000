b2bea3186c0a364bc52c4c963844fcee5cdd3b5c
Tayeb Mc <gomail654@gmail.com>
2017-02-16 16:05:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function changeCompletely(elements, index, array){
+  [4, 5, 6].forEach(function(changeCompletely){
+      console.log(elements);
+  });
+} 
+
+function doToElementsInArray(array, callback){
+   array.forEach(function(callback){
+     console.log(callback);
+   });
+}
+
